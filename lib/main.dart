26be9bf739c89b0c37246e4424fe1cd9f5728e51
Hyvos <daniel.liman07@gmail.com>
2024-02-24 19:04:28 +0700

    fix: Better Bottom Navbar

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class _MainAppState extends State<MainApp> {
     HomePage(),
     AboutYouPage(),
   ];
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -37,28 +37,48 @@ class _MainAppState extends State<MainApp> {
             });
           },
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _pageIndexNow,
-          onTap: (int index) {
-            setState(() {
-              _pageIndexNow = index;
-              _pageController.animateToPage(
-                index,
-                duration: Duration(milliseconds: 200),
-                curve: Curves.easeInOut,
-              );
-            });
-          },
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'About You',
-            ),
-          ],
+        bottomNavigationBar: Container(
+          height: 80,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(100),
+            boxShadow: const [
+              BoxShadow(
+              color: Color.fromARGB(24, 0, 0, 0),
+              blurRadius: 10,
+              offset: Offset(0, -1),
+              spreadRadius: 0,
+              )
+            ],
+          ),
+          child: BottomNavigationBar(
+            currentIndex: _pageIndexNow,
+            selectedItemColor: Color(0xFF5038bc),
+            showUnselectedLabels: false,
+            showSelectedLabels: false,
+            selectedIconTheme: IconThemeData(size: 40),
+            unselectedIconTheme: IconThemeData(size: 27),
+            onTap: (int index) {
+              setState(() {
+                _pageIndexNow = index;
+                _pageController.animateToPage(
+                  index,
+                  duration: Duration(milliseconds: 200),
+                  curve: Curves.easeInOut,
+                );
+              });
+            },
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'About You',
+              ),
+            ],
+          ),
         ),
       ),
     );
