2d46970d56b3f80c84151d461748043496f11e92
Hyvos <daniel.liman07@gmail.com>
2024-02-27 00:45:47 +0700

    feat: Make a Dedicated Class for Tasks Item

---
+++ lib/object/task.dart
@@ -0,0 +1,67 @@
+// ignore_for_file: prefer_final_fields
+
+import 'dart:ui';
+
+import 'package:todos/config/colorconfig.dart';
+
+class Task {
+  String? id; // ID will use "ID<milisecond from epoch time><random number>"
+  String? taskTitle;
+  bool taskDone;
+  DateTime startDate;
+  DateTime endDate;
+
+  Task({
+    required this.id,
+    required this.taskTitle,
+    required this.startDate,
+    required this.endDate,
+    this.taskDone = false, // Default value == Not done
+  });
+
+  static List<Task> todoList() {
+    return [];
+  }
+
+  String getDue() {
+    List<Object> due = decideDue();
+    String taskDue = due[0] as String;
+    String daysDifference = due[1] as String;
+
+    return "$taskDue $daysDifference";
+  }
+
+  Color getDueColor() {
+    List<Object> due = decideDue();
+    Color taskDueColor = due[2] as Color;
+
+    return taskDueColor;
+  }
+
+  List<Object> decideDue() {
+    DateTime now = DateTime.now();
+
+    DateTime nowDateDays = DateTime(now.year, now.month, now.day);
+
+    DateTime endDateDays = DateTime(endDate.year, endDate.month, endDate.day);
+
+    Duration difference = endDateDays.difference(nowDateDays);
+    String daysDifference = "${difference.inDays} days";
+
+    String taskDue = "This task is due for";
+    Color taskDueColor = const Color(0xFF5038BC);
+
+    if (difference.inDays == 0) {
+        daysDifference = "Today";
+        taskDueColor = cLightPurple;
+    } else if (now.isBefore(endDate)) {
+        daysDifference = "${difference.inDays} days";
+        taskDueColor = cLightPurple;
+    } else if (daysDifference.substring(0, 1) == "-") {
+        daysDifference = daysDifference.substring(1);
+        taskDue = "This task is already due for";
+        taskDueColor = cRed;
+    }
+    return [taskDue, daysDifference, taskDueColor];
+  }
+}
