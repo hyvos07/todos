42d9ea2b567c406261c9de10a25f2074ca327d18
Hyvos <daniel.liman07@gmail.com>
2024-02-27 00:44:06 +0700

    Add Color Configuration

---
+++ lib/config/colorconfig.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Color cRed = Color(0xFFDA4040);
+const Color cLightPurple = Color.fromARGB(255, 147, 145, 173);
+const Color cPurple = Color(0xFF5038BC);
+
+const Color cBlack = Color(0xFF3A3A3A);
+const Color cGrey = Color(0xFF717171);
+
+const Color cBGColor = Color(0xFFEEEFF5);
